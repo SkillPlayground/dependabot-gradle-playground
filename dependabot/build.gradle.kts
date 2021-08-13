54fe8766787a44d3d8f924d698dcd795ce00d933
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-08-13 13:54:23 +0200

    Add dependabot project

---
+++ dependabot/build.gradle.kts
@@ -0,0 +1,15 @@
+@file:Suppress("PropertyName", "VariableNaming")
+
+// catalog start
+
+// [versions]
+val kotlin = "1.5.21"
+
+// [libraries]
+val jetbrains_kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin"
+val jetbrains_kotlin_serialization = "org.jetbrains.kotlin:kotlin-serialization:$kotlin"
+
+// [bundles]
+val plugins_kotlin = jetbrains_kotlin_plugin + jetbrains_kotlin_serialization
+
+// catalog end
